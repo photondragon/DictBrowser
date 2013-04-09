2abd779c3e6af888c63539c1812afaddda141fa2
airhorse <photondragon@163.com>
2013-04-09 23:07:06 +0800

    add empty project

--- src/DictBrowser/DictBrowser/AppDelegate.m
+++ src/DictBrowser/DictBrowser/AppDelegate.m
@@ -23,7 +23,11 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
-	self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+	    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil] autorelease];
+	} else {
+	    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil] autorelease];
+	}
 	self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
