250b70219dd3f07cfd5b43b17ff82905e6ef805e
airhorse <photondragon@163.com>
2013-04-09 23:00:10 +0800

    add empty project

---
+++ src/DictBrowser/DictBrowser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DictBrowser
+//
+//  Created by mahj on 9/4/13.
+//  Copyright (c) 2013年 photondragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ src/DictBrowser/DictBrowser/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DictBrowser
+//
+//  Created by mahj on 9/4/13.
+//  Copyright (c) 2013年 photondragon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
